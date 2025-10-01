10513fa1c11bf7fbff4645621b39b3a9b6f83137
samsumeet <sumeet.1991@gmail.com>
2025-10-02 01:32:53 +0200

    Initial Commit

---
+++ TradePro/TradeProApp.swift
@@ -0,0 +1,20 @@
+//
+//  TradeProApp.swift
+//  TradePro
+//
+//  Created by Sumeet Bachchas on 02.10.25.
+//
+
+import SwiftUI
+
+@main
+struct TradeProApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
